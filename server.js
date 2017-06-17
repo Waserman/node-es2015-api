4d59b26d8a35d328eb6c8737083d3a8c3031cb1f
Waserman <waserman2302@gmail.com>
2017-06-17 14:16:10 +0300

    database connection

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 import app from './config/express'
-const port = process.env.PORT || 5000
+import config from './config/env'
+import mongoose from 'mongoose'
 
-app.listen(port, () => {
-    console.log(`app is runing on port ${port}`)
+
+mongoose.connect(config.db)
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${config.db}`)
 })
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to database: ${config.db}`)
+})
+
+if (config.env === 'development') {
+  mongoose.set('debug', true)
+}
+
+
+app.listen(config.port, () => {
+  console.log(`API Server started and listening on port ${config.port} (${config.env})`);
+})
+
+export default app
